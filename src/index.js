da903336ed5e29f59411b33ce3ceb62d26a33ce5
Roman Stankiewicz <roman.stankiewicz@perkbox.com>
2019-06-29 21:34:02 +0100

    tidy

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import {render} from "react-dom"
 import {Provider} from "react-redux"
 import {ConnectedRouter} from "connected-react-router"
+
 import store, {history} from "./store"
 import App from "./containers/App"
-
 import "./index.css"
 
 const target = document.querySelector("#root")
